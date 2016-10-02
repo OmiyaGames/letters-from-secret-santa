665ab0c5eac335f3ba2cf94abe507a9df9bfbd20
Taro Omiya <taro.omiya@omiyagames.com>
2016-10-01 21:04:52 -0400

    Adding standard Assets.

---
+++ Assets/Standard Assets/Omiya Games/Scripts/TriggerMessage.cs
@@ -0,0 +1,41 @@
+/*
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TriggerMessage : MonoBehaviour
+{
+	public string message;
+	
+	void Awake()
+	{
+		collider.isTrigger = true;
+	}
+	
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player") == true)
+		{
+            // Grab the controller
+            ThirdPersonUserControl controller = other.GetComponent<ThirdPersonUserControl>();
+            if((controller != null) && (controller.characterController.indicator == ThirdPersonCharacter.Indicator.Controlled))
+            {
+                PauseMenu.ShowMessage(message);
+            }
+		}
+	}
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player") == true)
+        {
+            // Grab the controller
+            ThirdPersonUserControl controller = other.GetComponent<ThirdPersonUserControl>();
+            if((controller != null) && (controller.characterController.indicator == ThirdPersonCharacter.Indicator.Controlled))
+            {
+                PauseMenu.HideMessage();
+            }
+        }
+    }
+}
+*/
